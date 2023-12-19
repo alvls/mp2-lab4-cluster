d8c68fb5936e0c1e1e90d09cf84c23bf190b97fd
Gn4ik <112872726+Gn4ik@users.noreply.github.com>
2023-12-19 18:43:31 +0300

    несколько тестов очереди

--- test_queue.cpp
+++ test_queue.cpp
@@ -26,3 +26,29 @@ TEST(TQueue, can_get_count_of_elem) {
 TEST(TQueue, throw_when_the_length_too_much) {
 	ASSERT_ANY_THROW(TQueue<int> qu(100000));
 }
+
+TEST(TQueue, dequeue_throws_when_queue_isEmpty) {
+	TQueue<int> qu;
+	EXPECT_ANY_THROW(qu.dequeue());
+}
+
+TEST(TQueue, peek_throw_when_queue_isEmpty) {
+	TQueue<int> qu;
+	EXPECT_ANY_THROW(qu.peek());
+}
+
+TEST(TQueue, size_is_changing_after_enqueue) {
+	TQueue<int> qu;
+	EXPECT_EQ(0, qu.Size());
+	qu.enqueue(1);
+	EXPECT_EQ(1, qu.Size());
+}
+
+TEST(TQueue, size_is_changing_after_dequeue) {
+	TQueue<int> qu;
+	qu.enqueue(1);
+	qu.enqueue(2);
+	EXPECT_EQ(2, qu.Size());
+	qu.dequeue();
+	EXPECT_EQ(1, qu.Size());
+}
